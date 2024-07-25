5e50d51a209449869bd79dd56881a3327baefe32
Ardiansyah Putra <putraardiansyah530@gmail.com>
2024-07-25 19:44:46 +0700

    Update: AboutUs - Mobile

--- resources/views/livewire/about-us.blade.php
+++ resources/views/livewire/about-us.blade.php
@@ -1,5 +1,5 @@
-<div class="grid grid-cols-7 mt-10 mb-16 gap-10">
-    <div class="col-span-3 text-black flex flex-col justify-center">
+<div class="grid grid-cols-7 mt-0 mb-7 md:mt-10 md:mb-16 gap-10">
+    <div class="col-span-7 md:col-span-3 text-black flex flex-col justify-center">
         <span class="inline-block text-4xl font-bold">TENTANG KAMI</span>
         <span class="inline-block mt-5 font-semibold">PT Alfajri Creative Agency adalah perusahaan kreatif yang
             memberikan layanan
@@ -12,7 +12,7 @@
             tim yang berpengalaman, kami berkomitmen untuk membantu Anda mencapai tujuan di pasar yang kompetitif
             ini.</span>
     </div>
-    <div class="col-span-4">
+    <div class="col-span-7 md:col-span-4">
         <img src="https://picsum.photos/700/400" class="rounded-3xl shadow-lg shadow-black">
     </div>
 </div>
