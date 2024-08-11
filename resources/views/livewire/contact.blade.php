433789cfda73f433060e534f688a7d179d889072
Ardiansyah Putra <putraardiansyah530@gmail.com>
2024-08-11 11:14:53 +0700

    Update: GoogleMaps

--- resources/views/livewire/contact.blade.php
+++ resources/views/livewire/contact.blade.php
@@ -27,6 +27,9 @@
         </div>
     </div>
     <div class="col-span-7 md:col-span-4">
-        <img src="{{ asset('assets/img/call-us-banner.png') }}" class="rounded-3xl shadow-lg shadow-black">
+        <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d247.8879918295379!2d106.70531906164828!3d-6.236308814957611!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69fa16a98e2e1d%3A0x9b2d65213bd9bf48!2sPT%20ALFAJRI%20CREATIVE%20AGENCY%2C%20AF%20Printing%20%26%20AOS!5e0!3m2!1sid!2sid!4v1723349590210!5m2!1sid!2sid"
+            width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade"></iframe>
     </div>
 </div>
