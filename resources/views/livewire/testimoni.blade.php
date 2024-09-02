0aa151b1739c0c75f50e02710dae57d13e235c23
Ardiansyah Putra <putraardiansyah530@gmail.com>
2024-09-02 12:49:54 +0700

    Update: Testimoni Pafe

--- resources/views/livewire/testimoni.blade.php
+++ resources/views/livewire/testimoni.blade.php
@@ -1,3 +1,126 @@
 <div>
-    The Master doesn't talk, he acts.
+    <div class="flex justify-center gap-10 mt-10 mb-24">
+        <div class="font-bold text-5xl underline text-black dark:text-white flex items-center">
+            TESTIMONI
+        </div>
+        <div class="grid grid-cols-6 grid-rows-2 w-2/3 h-[500px]">
+            <div class="col-span-1 row-span-2 hover:scale-y-110">
+                <img class="w-full h-full object-none" src="{{ asset('assets/img/digital-design-banner2.png') }}">
+            </div>
+            <div class="bg-green-400 col-span-1 row-span-2 hover:scale-y-110">
+                <img class="w-full h-full object-none" src="{{ asset('assets/img/digital-design-banner2.png') }}">
+            </div>
+            <div class="bg-green-500 col-span-1 row-span-2 hover:scale-y-110">
+                <img class="w-full h-full object-none" src="{{ asset('assets/img/digital-design-banner2.png') }}">
+            </div>
+            <div class="bg-green-600 col-span-1 row-span-2 hover:scale-y-110">
+                <img class="w-full h-full object-none" src="{{ asset('assets/img/digital-design-banner2.png') }}">
+            </div>
+            <div class="bg-green-700 col-span-1 row-span-2 hover:scale-y-110">
+                <img class="w-full h-full object-none" src="{{ asset('assets/img/digital-design-banner2.png') }}">
+            </div>
+            <div class="bg-green-800 col-span-1 row-span-2 hover:scale-y-110">
+                <img class="w-full h-full object-none" src="{{ asset('assets/img/digital-design-banner2.png') }}">
+            </div>
+        </div>
+    </div>
+    <div>
+        <div class="bg-green-200 pt-10 flex">
+            <div class="w-1/3 flex items-end">
+                <img src="{{ asset('assets/img/testimoni-ilustrasi-1.png') }}">
+            </div>
+            <div class="w-2/3 flex items-center justify-center">
+                <div class="max-w-xl text-center">
+                    <p class="text-4xl font-semibold mb-10">
+                        "Pelayanan baik, mampu membuat design spanduk sesuai keinginan pelanggan, produksi cepat, dan
+                        tentunya bahannya bagus"
+                    </p>
+                    <p class="text-xl font-semibold">
+                        • Pak E****
+                    </p>
+                    <p class="text-xl font-semibold">
+                        (Berlangganan 3 Tahun Lebih)
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="bg-sky-300 pt-10 flex">
+            <div class="w-2/3 flex items-center justify-center">
+                <div class="max-w-3xl text-center">
+                    <p class="text-2xl font-semibold mb-10 p-5">
+                        "Selama sy menjadi pelanggan Alfajri Creative Agancy (AFCA) memuaskan yah, hasilnya bagus tepat
+                        waktu yg terpentin informatif serta komunikatif jadi apa yg kita tanya mau kita butuhkan bisa
+                        terbantu dg baik dan tepat sehingga berjalan dg baik, alhamdulillah selama lebih kurang 10 tahun
+                        yah sy berlangganan dr yg awalnya punya mesin cetak yg besar sampe sekarang ada mesin cetak yg
+                        kecil tuh, ditambah lagi pekerja2 yg gercep pastinya ngebuat setiap job selasai
+                        terimaksih yah AFCA udah banyak bantu kita
+                        oya, 1 lagi ini yg gak kalah penting
+                        harganya terjangkau kok"
+                    </p>
+                    <p class="text-xl font-semibold">
+                        • Pak F****
+                    </p>
+                    <p class="text-xl font-semibold">
+                        (Berlangganan 10 Tahun Lebih)
+                    </p>
+                </div>
+            </div>
+            <div class="w-1/3 flex items-end">
+                <img src="{{ asset('assets/img/testimoni-ilustrasi-2.png') }}">
+            </div>
+        </div>
+        <div class="bg-yellow-300 pt-10 flex">
+            <div class="w-1/3 flex items-end">
+                <img src="{{ asset('assets/img/testimoni-ilustrasi-3.png') }}">
+            </div>
+            <div class="w-2/3 flex items-center justify-center">
+                <div class="max-w-xl text-center">
+                    <p class="text-4xl font-semibold mb-10 p-5">
+                        "Pelayanan Ramah, Baik, Cepat.. Desain yang di tawarkan Menarik, Desain sesuai dengan keinginan
+                        konsumen, Waktu
+                        Cetak Juga cepat.."
+                    </p>
+                    <p class="text-xl font-semibold">
+                        • Pak S****
+                    </p>
+                    <p class="text-xl font-semibold">
+                        (Berlangganan 6 Tahun Lebih)
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="bg-rose-400 pt-10 flex">
+            <div class="w-2/3 flex items-center justify-center">
+                <div class="max-w-xl text-center">
+                    <p class="text-4xl font-semibold mb-10 p-5">
+                        "Untuk pelayanan yang saya rasakan selama ini cukup BAIK ya."
+                    </p>
+                    <p class="text-xl font-semibold">
+                        • Pak D****
+                    </p>
+                    <p class="text-xl font-semibold">
+                        (Berlangganan 3 Tahun Lebih)
+                    </p>
+                </div>
+            </div>
+            <div class="w-1/3 flex items-end">
+                <img src="{{ asset('assets/img/testimoni-ilustrasi-4.png') }}">
+            </div>
+        </div>
+        <div class="bg-green-400 pt-10 flex">
+            <div class="w-1/3 flex items-end">
+                <img src="{{ asset('assets/img/testimoni-ilustrasi-5.png') }}">
+            </div>
+            <div class="w-2/3 flex items-center justify-center">
+                <div class="max-w-xl text-center">
+                    <p class="text-4xl font-semibold mb-10 p-5">
+                        "Memuaskan"
+                    </p>
+                    <p class="text-xl font-semibold">
+                        • Pak E****
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
